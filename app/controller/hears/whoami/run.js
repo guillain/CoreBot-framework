a6885fd6730fc2428db509367d84bb56493420d6
Guillain Sanchez (Europe) <guillain.sanchez@dimensiondata.com>
2018-12-16 23:24:42 +0100

    hears whoami default conf

---
+++ app/controller/hears/whoami/run.js
@@ -0,0 +1,17 @@
+// Configuration
+let config = require('../../../config');
+
+// Exports controller function as scenario
+exports.run = function(bot, message, controller, config) {
+    if (config.controller.hears.whoami.enable === true) {
+        controller.hears('who am i', ['direct_message', 'direct_mention'], function (bot, message) {
+            bot.reply(message,
+                'You are ' + message.user
+                + ' and your email is ' + message.data.personEmail
+                + ' and your user id is ' + message.data.personId);
+        });
+    }
+    return controller;
+};
+
+
