b6bd0724edd4b13e786d64b52620bfe554e19fab
Guillain Sanchez (Europe) <guillain.sanchez@dimensiondata.com>
2018-12-16 23:24:58 +0100

    Add message in the params

--- app/controller/hears/echotest/run.js
+++ app/controller/hears/echotest/run.js
@@ -2,7 +2,7 @@
 let stz = require('../../../component/stanza/run.js');
 
 // Exports controller function as scenario
-exports.run = function(bot, controller, config) {
+exports.run = function(bot, message, controller, config) {
     if (config.controller.hears.echotest.enable === true) {
         controller.hears('^test', ['direct_mention', 'direct_message', 'self_message'], function (bot, message) {
             stz.msg(bot, message);
