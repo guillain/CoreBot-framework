db7b95291f6953e0ea51ee2922481a02ae7fbb17
Guillain Sanchez (Europe) <guillain.sanchez@dimensiondata.com>
2018-12-16 23:17:48 +0100

    hears echotest default conf

--- app/controller/hears/echotest/run.js
+++ app/controller/hears/echotest/run.js
@@ -1,17 +1,12 @@
-// Configuration
-let config = require('../../config');
-
 // Load the required libraries
-let stz = require('../../component/stanza/run.js');
+let stz = require('../../../component/stanza/run.js');
 
 // Exports controller function as scenario
-exports.run = function(controller) {
-    if (config.hears.echotest === 1) {
+exports.run = function(bot, controller, config) {
+    if (config.controller.hears.echotest.enable === true) {
         controller.hears('^test', ['direct_mention', 'direct_message', 'self_message'], function (bot, message) {
             stz.msg(bot, message);
         });
     }
     return controller;
 };
-
-
