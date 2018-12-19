12202ee56bb7f749883066b76e93bab459433188
guillain <guillain@gmail.com>
2018-12-19 22:41:14 +0000

    fix echo

--- app/controller/hears/echotest/run.js
+++ app/controller/hears/echotest/run.js
@@ -4,9 +4,10 @@ let stz = require(__basedir + 'component/stanza/run.js');
 // Exports controller function as scenario
 exports.run = function(controller, config) {
     if (config.controller.hears.echotest.enable === true) {
-        controller.hears('^test', ['direct_mention', 'direct_message', 'self_message'], function (bot, message) {
-            bot.reply(message, message.text);
-            // stz.msg(bot, message);
+        controller.hears('^echotest', ['direct_mention', 'direct_message', 'self_message'], function (bot, message) {
+            if (message.user !== config.user) {
+                bot.reply(message, message.text);
+            }
         });
     }
     return controller;
