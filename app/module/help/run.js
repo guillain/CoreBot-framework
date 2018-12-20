0f9e828d8f09cb7cfd6187c17d7b5e12d4e114bd
guillain <guillain@gmail.com>
2018-12-20 17:43:55 +0000

    v1 done

--- app/module/help/run.js
+++ app/module/help/run.js
@@ -12,9 +12,8 @@ exports.run = function(bot, message, config){
     let to_say = '';
     let msg_arr = message.text.split(' ');
 
-    // Set if detail is requested
-    let detail = false;
-    if (/detail/i.test(message.text)) detail = true;
+    let all = msg_arr.indexOf('all');
+    let detail = msg_arr.indexOf('detail');
 
     // Reject if not Help command
     if (!/^help/i.test(message.text)) {
@@ -25,30 +24,39 @@ exports.run = function(bot, message, config){
     else if (message.user === config.user) {
         tools.debug("info", 'module help run stop_bot_reply');
     }
-
-    // Help of a module is requested
-    else if ((msg_arr.length > 1) && (!/detail/i.test(message.text))) {
+    // If help summary is requested
+    else if (msg_arr.length === 1){
+        _.each(config.module, function (conf, index) {
+            if (config.module[index].enable === true)
+                to_say += '- '+index+' - '+config.module[index].msg.help[0] + '\n';
+        });
+    }
+    // If all help are requested
+    else if ((msg_arr.length > 1) && ((all >= 0) || (detail >=0))){
+        _.each(config.module, function (conf, index) {
+            if (detail < 0) {
+                to_say += '- '  + index;
+                if (all >= 0) to_say += ' - state:_' + config.module[index].enable + '_';
+                to_say += ' - ' + config.module[index].msg.help[0] + '\n';
+            }
+            else if (detail >= 0) {
+                to_say += '# '+index+'\n';
+                to_say += 'State: _'+ config.module[index].enable + '_\n\n';
+                to_say += config.module[index].msg.help.join('\n\n') + '\n\n';
+            }
+        });
+    }
+    // Help of a module is requested (it includes detail)
+    else if ((msg_arr.length > 1) && (all >= 0)) {
         let res = _.each(config.module, function (conf, index) {
-            if (index === msg_arr[1]) found = index;
+            if (msg_arr.indexOf(index) >= 0) found = index;
         });
         if (found !== '')
             to_say = config.module[found].msg.help.join('\n');
         else
-            to_say = to_say = config.module.help.msg.nomodule;
+            to_say = config.module.help.msg.nomodule;
+    }
 
-        bot.reply(message, to_say);
-    }
-    // If all all help are requested
-    else if ((msg_arr.length == 1) || (!/detail/i.test(message.text))) {
-        _.each(config.module, function (conf, index) {
-            if (config.module[index].enable === true) {
-                if (msg_arr.length == 1)
-                    to_say += '- '+index+' - '+config.module[index].help + '\n';
-                else
-                    to_say += '#'+index+'\n'+config.module[index].help + '\n';
-            }
-            bot.reply(message, to_say);
-        });
-    }
+    if (to_say) bot.reply(message, to_say);
 };
 
