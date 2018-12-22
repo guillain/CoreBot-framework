99210d15c8a0a7810dd94bfd03e4bead4be132b2
guillain <guillain@gmail.com>
2018-12-22 18:13:19 +0000

    Search engine improved

--- app/module/search/run.js
+++ app/module/search/run.js
@@ -18,6 +18,13 @@ exports.run = function(bot, message, config){
 
     // Remove first pattern if: present + prefixed=true
     let msg_arr = message.text.split(' ');
+
+    // Variables for the search engine
+    let i_total = i_arr = i_km = i_key = counter = 0;
+    let res = re = keys_words = '';
+    let msg_arr_key = res_i_km = [];
+    let tosay = config.module.search.msg.notfound;
+
     if (!(/^search$/i.test(msg_arr['0'])) && (config.module.search.prefixed === true)){
         tools.debug('debug', 'module search run return');
         return;
@@ -31,20 +38,70 @@ exports.run = function(bot, message, config){
     if (/^help$/i.test(msg_arr['0']))
         bot.reply(message, config.module.search.msg.help.join('\n'));
     else {
-        let tosay = config.module.search.msg.notfound;
         client.hgetall(config.module.search.storage, function(err,kms){
-            let j = 0;
-            let res = '';
             if (err) throw err;
-            for (let i in kms) {
-                let re =  new RegExp('\\b'+ msg_arr.join(' ') + '\\b','i');
-                if(re.exec(kms[i])) { 
-                    if (j < config.module.search.limit) { res += '- '+kms[i]+'\n'; }
-                    j++;
+
+            // Key array initilization
+            for (i_arr in msg_arr) if (msg_arr[i_arr].length >= config.module.search.key_length) msg_arr_key[i_key++] = msg_arr[i_arr];
+            let msg_arr_key_len = msg_arr_key.length;
+            tools.debug('debug', 'module search run msg_arr ' + msg_arr);
+
+            if (msg_arr_key_len !== 0) {
+
+                // First search with all keys
+                //let re =  new RegExp('\\b'+ msg_arr_key.join('(.)*') + '\\b','i');
+                let re =  new RegExp(msg_arr_key.join('(.)*'),'i');
+                for (i_km in kms) {
+                    if(re.exec(kms[i_km])) {
+                        if (i_total < config.module.search.limit) {
+                            tools.debug('debug', 'module search run msg_arr_key.join ' + msg_arr_key.join(' '));
+                            res += '- '+i_km+' - '+kms[i_km]+' `key words(1): '+msg_arr_key.join(' ')+'`\n'; 
+                            res_i_km[i_total++] = i_km;
+                        }
+                        else i_total++;
+                    }
                 }
+
+                tools.debug('debug', 'module search run i_total ' + i_total);
+                tools.debug('debug', 'module search run res_i_km ' + res_i_km);
+    
+                // Complete the result with additionnal sub search
+                if (i_total < config.module.search.limit){
+                    for (i_km in kms) {
+                        tools.debug('debug', 'module search run kms['+i_km+']' + kms[i_km]);
+
+                        if (res_i_km.indexOf(i_km) < 0) {
+                            keys_words = '';
+                            counter = 0;
+                            for (i_key=0; i_key < msg_arr_key_len; i_key++) {
+                                tools.debug('debug', 'module search run msg_arr['+i_key+'] ' + msg_arr_key[i_key] + ' ' + i_km);
+
+                                let re =  new RegExp(msg_arr_key[i_key],'i');
+                                if (re.exec(kms[i_km])) {
+                                    tools.debug('debug', 'module search run FOUND');
+                                    counter++; 
+                                    keys_words += ' '+msg_arr_key[i_key];
+                                }
+                            }
+                            if (counter >= config.module.search.sub_limit){
+                                tools.debug('debug', 'module search run i_total ' + i_total + ' ' + msg_arr_key);
+
+                                if (i_total < config.module.search.limit) {
+                                    res += '- '+i_km+' - '+kms[i_km]+' `key words(2.'+counter+'): '+keys_words+'`\n';
+                                    //res_i_km[i_total++] = i_km;
+                                    i_total++;
+                                }
+                                else i_total++;
+                                tools.debug('debug', 'module search run i_total ' + i_total + ' ' + msg_arr_key);
+                            }
+                        }
+                    }
+                }
+                if (i_total > 0) tosay = config.module.search.msg.found + ': ' + i_total;
+                if (i_total > config.module.search.limit) tosay += ' but '+config.module.search.limit+' displayed';
             }
-            if (j > 0) tosay = config.module.search.msg.found + ': ' + j;
-            if (j > config.module.search.limit) tosay += ' but '+config.module.search.limit+' displayed';
+            else to_say = config.module.search.msg.too_short;
+
             bot.reply(message, tosay + '\n' + res);
         });
         /*
