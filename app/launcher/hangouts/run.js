475e937b38c92784491a825049d7c46c3f3bbc81
guillain <guillain@gmail.com>
2018-12-22 07:58:44 +0000

    prod val

--- app/launcher/hangouts/run.js
+++ app/launcher/hangouts/run.js
@@ -2,24 +2,27 @@
 let tools = require(__basedir + 'lib/tools');
 
 // Configuration
-let Botkit = require(__basedir + 'botkit/lib/Botkit.js');
+let Botkit = require('botkit');
 
 // Controller
 exports.run = function(config) {
-  tools.debug('debug', 'launcher hangouts run");
+  tools.debug('debug', 'launcher hangouts run');
+
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = __basedir + config.launcher.hangouts.json_cred;
 
   let controller = Botkit.googlehangoutsbot({
     endpoint: config.launcher.hangouts.endpoint,
     token: config.launcher.hangouts.token,
     debug: config.log.debug,
-    studio_token: config.controller.on.botkit.token
+    studio_token: config.controller.on.botkit.token,
+    json_file_store: __basedir + config.launcher.hangouts.store
   });
 
   let bot = controller.spawn({})
 
   controller.setupWebserver(config.launcher.hangouts.port || 3002, function (err, webserver) {
     controller.createWebhookEndpoints(webserver, bot, function () {
-      tools.debug('info', 'launcher hangouts run ok");
+      tools.debug('info', 'launcher hangouts run ok');
     });
   });
 
