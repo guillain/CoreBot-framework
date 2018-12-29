5699a79bf4f4bcf5da2684c99f9e787a6b842d5c
guillain <guillain@gmail.com>
2018-12-29 03:37:23 +0000

    Split conf file

--- app/CoreBot-framework.js
+++ app/CoreBot-framework.js
@@ -1,14 +1,13 @@
 // Configuration
 global.__basedir = __dirname  + '/';
+
+// Load required library
 let Log = require(global.__basedir + 'lib/log');
-let conf = require(global.__basedir + 'conf/config.json');
 
 Log.debug('main start');
-//var debug = require('debug')('CoreBot-framework:main');
 
 // Run the launchers
-let launcher = require(global.__basedir + 'lib/launcher.js');
-launcher.run(conf.launcher);
+require(global.__basedir + 'lib/launcher.js')();
 
 Log.debug('main done');
 
